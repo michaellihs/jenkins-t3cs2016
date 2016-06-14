51b6d1a60bff327f9c806f839decac11eb2db62f
Michael Lihs <mimi@kaktusteam.de>
2016-06-14 22:12:59 +0200

    [TASK] Install composer

--- cookbooks/env_typo3jenkins/recipes/_composer.rb
+++ cookbooks/env_typo3jenkins/recipes/_composer.rb
@@ -1,11 +1,12 @@
-# TODO install composer with the "3 lines" from composer website
+composer_install_script = <<-EOSCRIPT
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
+  sudo mv composer.phar /usr/local/bin/composer
+EOSCRIPT
 
-=begin
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
-
-=end
+execute 'Install composer' do
+  command composer_install_script
+  not_if { File.exists? '/usr/local/bin/composer' }
+end
