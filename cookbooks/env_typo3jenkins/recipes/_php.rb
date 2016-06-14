edf8df8c3cbb5ee39335da2203fd0f2acf3df1d8
Michael Lihs <mimi@kaktusteam.de>
2016-06-14 21:51:43 +0200

    [TASK] Install required PHP packages

--- cookbooks/env_typo3jenkins/recipes/_php.rb
+++ cookbooks/env_typo3jenkins/recipes/_php.rb
@@ -1,5 +1,3 @@
-# TODO install required PHP modules
-
-=begin
-sudo apt-get install -y php5-cli php5-curl php5-gd php5-gmp php5-imap php5-intl php5-mcrypt php5-mysqlnd php5-pspell php5-readline php5-recode php5-sqlite php5-xmlrpc php5-xsl
-=end
+%w(php5-cli php5-curl php5-gd php5-gmp php5-imap php5-intl php5-mcrypt php5-mysqlnd php5-pspell php5-readline php5-recode php5-sqlite php5-xmlrpc php5-xsl).each do | php_package |
+  package php_package
+end
